89158688a658b8f7dd763651e0304f6258a07816
Luca <lucaporzio2004@gmail.com>
2023-05-14 19:22:43 +0200

    Fixed mobile view

--- profile.php
+++ profile.php
@@ -3,8 +3,11 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Profilo</title>
-    <link rel="stylesheet" type="text/css" href="styles.css">
+    <link rel="icon" type="image/gif" href="Icons/Logo Progetto Gpoi V2.png">
+    <link rel="stylesheet" href="styles.css">
 
     <style>
         .form-field {
@@ -64,7 +67,8 @@
 
         <button class="login-btn" id="log-out" onclick='window.location.href = "logout.php"'
             tex-align="center">Log-out</button>
-        <button type="button" id="sign-in" class="login-btn" onclick='window.location.href = "index.php"'> Homepage</button>
+        <button type="button" id="sign-in" class="login-btn" onclick='window.location.href = "index.php"'>
+            Homepage</button>
     </div>
 </body>
 
